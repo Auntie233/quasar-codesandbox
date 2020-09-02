e161a6d9ad77d3c5fddcb04cefd3102f24159ff5
hahama01 <hahama01@gmail.com>
2020-09-02 18:29:21 +0800

    init

---
+++ src/layouts/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-list>
+        <q-item to="/" exact>
+            <q-item-section avatar>
+                <q-icon name="inbox"/>
+            </q-item-section>
+
+            <q-item-section>
+                Index
+            </q-item-section>
+        </q-item>
+    </q-list>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
